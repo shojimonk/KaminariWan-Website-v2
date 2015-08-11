ad48ac470087228d545821c1d38f9c98c5951acd
shojimonk <shojimonk@gmail.com>
2015-08-11 15:45:29 -0400

    Re-creation of source control to resolve gitignore issues.
    Re-creation of source control to resolve gitignore issues.

---
+++ Kaminari-Wan-Taiko-Website/Kaminari-Wan-Taiko-Website.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kaminari_Wan_Taiko_Website.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Kaminari-Wan-Taiko
+simple website for a local Thunder Bay Taiko (japanese-style drumming) group.
